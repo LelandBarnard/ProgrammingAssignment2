b12970f8c6f5649243a02749783cc05faff798f1
LelandBarnard <leland.barnard@gmail.com>
2017-01-21 20:29:28 -0500

    assignment 2 commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These are functions for creating a matrix object that has methods for 
+##initializing the matrix, returning the value of the matrix, and for initializing
+##and returning the matrix inverse.
 
-## Write a short comment describing this function
+## This function creates the matrix object with the following methods:
+## set: This initializes the matrix X with the argument Y.
+## get: This returns the value of the matrix.
+## setivrs: This initializes the matrix inverse with argument ivrs.
+## getivrs: This returns the value of the matrix inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(X = matrix()) {
+  Xivrs <- NULL
+  set <- function(Y) {
+    X <<- Y
+    Xivrs <<- NULL
+  }
+  get <- function() X
+  setivrs <- function(ivrs) Xivrs <<- ivrs
+  getivrs <- function() Xivrs
+  list(set = set, get = get,
+       setivrs = setivrs,
+       getivrs = getivrs)
 }
 
 
-## Write a short comment describing this function
+## This matrix first checks to see if the matrix inverse has been computed and stored.
+## If it has, it retrieves the inverse from the cache.  If it has not, it computes and
+## stores it in the cache.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(X, ...) {
         ## Return a matrix that is the inverse of 'x'
+  Xivrs <- X$getivrs()
+  if(!is.null(Xivrs)){
+    message("getting cached inverse")
+    invisible(Xivrs)
+  }
+  data <- X$get()
+  Xivrs <- solve(data)
+  X$setivrs(Xivrs)
 }
